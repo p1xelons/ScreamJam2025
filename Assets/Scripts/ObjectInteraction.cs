cb4914b9833fda6625159ee95781b133259a9866
dm1088 <dm1088@rit.edu>
2025-10-09 15:00:47 -0400

    Added test scene/scripts

---
+++ Assets/Scripts/ObjectInteraction.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectInteraction : MonoBehaviour
+{
+
+    private List<GameObject> inventory;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
