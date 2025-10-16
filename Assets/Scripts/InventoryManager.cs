22b1cffae123cc4cf2cf2c2c60d38c43a188751f
Sam Hager <93140106+p1xelons@users.noreply.github.com>
2025-10-15 23:12:12 -0400

    made inventory script

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,99 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+public class Inventory
+{
+    public List<string> inventoryItems;
+
+    public Inventory()
+    {
+        inventoryItems = new List<string>();
+    }
+}
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+    public Inventory inventory;
+    private string filePath;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        filePath = Path.Combine(Application.persistentDataPath, "inventory.json");
+
+        // load inventory
+        LoadInventory();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// adds item to inventory
+    /// </summary>
+    /// <param name="itemName">name of item to add</param>
+    public void AddItem(string itemName)
+    {
+        if (!inventory.items.Contains(itemName))
+        {
+            inventory.items.Add(itemName);
+            SaveInventory();
+        }
+    }
+
+    /// <summary>
+    /// removes item from inventroy
+    /// </summary>
+    /// <param name="itemName">name of item to add</param>
+    public void RemoveItem(string itemName)
+    {
+        if (inventory.items.Contains(itemName))
+        {
+            inventory.items.Remove(itemName);
+            SaveInventory();
+        }
+    }
+
+    /// <summary>
+    /// save to the file
+    /// </summary>
+    private void SaveInventory()
+    {
+        string json = JsonUtility.ToJson(inventory);
+        File.WriteAllText(filePath, json);
+    }
+
+    /// <summary>
+    /// load from the file
+    /// </summary>
+    private void LoadInventory()
+    {
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            inventory = JsonUtility.FromJson<Inventory>(json);
+        }
+        else
+        {
+            // inventory = new Inventory();
+        }
+    }
+}
